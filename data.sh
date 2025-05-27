6a748aa08d77bb5c5b57250ce86ca1cd08567916
mohamed9087-om <Mohammedhsn905@gmail.com>
2025-05-27 14:02:56 +0300

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 read -p " Enter the username " username
 read -p " Enter the full name " fullname
 
 info=$username,$fullname
-CSV_FILE=employee.csv
+
 read -p  "Please, Confirm from data you entered. Is it correct ?[y/n]" input
 
 case $input in 
 	yes | Yes )
-		echo "$info" >> $CSV_FILE
+		echo "$info" >> employee.csv
 		echo " Your data has been stored successfully " 
 		exit 0
 		;;
